6e91037a3041bd25cb066885bcff19c69b883fc6
Donald Bales <don@donaldbales.com>
2024-12-27 09:04:00 -0700

    Cleanup of app.js

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var gdataRouter = require('./routes/garden_data');
 
-//const mysql = require('mysql');
-
 var app = express();
 
 // view engine setup
@@ -27,17 +25,15 @@ app.use('/', indexRouter);
 app.use('/', gdataRouter);
 
 // catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  next(createError(404));
-//});
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-                                                            // was {}
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
